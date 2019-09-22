5640b02bec575ded273d71edde0160344fd2e8f5
James Mobus <mobusj@live.com>
2019-09-22 18:58:00 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,7 +15,7 @@ class Doctor
     @@all
   end 
   
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     @appointments << Appointment.new(date, patient, self)
   end
 end
