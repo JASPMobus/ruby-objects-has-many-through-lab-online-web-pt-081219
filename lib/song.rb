b80a6f725c0340bd449b91e7dd4de2001a91c2d8
James Mobus <mobusj@live.com>
2019-09-22 18:42:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre
+    
+    artist.genres.push(genre)
+    
+    @@all << self
+  end
+end 
